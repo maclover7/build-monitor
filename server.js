b86a4da4a52a8c3c0c24eafd3fc8edec5344ac59
Jon Moss <me@jonathanmoss.me>
2017-11-06 13:39:38 -0500

    initial MVP

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/computer', function(req, res) {
+  request('https://ci.nodejs.org/computer/api/json?pretty=true').pipe(res);
+});
+
+app.get('/queue', function(req, res) {
+  request('https://ci.nodejs.org/queue/api/json').pipe(res);
+});
+
+app.listen(3000, function() {
+  console.log('Listening...');
+});
