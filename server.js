e57fffa6a0f605476ca609e9dda187e69504944f
Jon Moss <me@jonathanmoss.me>
2017-11-08 20:09:20 -0500

    Update variable names

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/computer', function(req, res) {
-  request({ url: 'https://ci.nodejs.org/computer/api/json?pretty=true', json: true }, (err, _, apiResponse) => {
+  request({ url: 'https://ci.nodejs.org/computer/api/json?pretty=true', json: true }, (err, response, body) => {
     var offlineComputers = [];
 
-    for(var computer of apiResponse.computer) {
+    for(var computer of body.computer) {
       if (computer.offline) {
         offlineComputers.push({
           name: computer.displayName,
@@ -21,19 +21,19 @@ app.get('/computer', function(req, res) {
     res.json({
       computers: offlineComputers,
       offlineCount: offlineComputers.length,
-      totalCount: apiResponse.computer.filter((c) => { return c._class === 'hudson.slaves.SlaveComputer' }).length
+      totalCount: body.computer.filter((c) => { return c._class === 'hudson.slaves.SlaveComputer' }).length
     });
   });
 });
 
 app.get('/queue', function(req, res) {
-  request({ url: 'https://ci.nodejs.org/queue/api/json', json: true }, (err, _, apiResponse) => {
+  request({ url: 'https://ci.nodejs.org/queue/api/json', json: true }, (err, response, body) => {
     // Create an array with list of "reasons why" jobs are in queue.
     // Then create a map which counts the number of time each reason appears.
     // Sorted with highest number of common reason at the top, and decreases after.
     var queue = [];
 
-    for(var item of apiResponse.items) {
+    for(var item of body.items) {
       var existingItem = queue.filter(function(i) {
         return i.reason === item.why;
       })[0];
