37e42d91f2c1c50861fab5246084e70558f0291a
Jon Moss <me@jonathanmoss.me>
2017-11-08 20:01:01 -0500

    Move /computer logic to backend

--- server.js
+++ server.js
@@ -6,7 +6,24 @@ const path = require('path');
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/computer', function(req, res) {
-  request('https://ci.nodejs.org/computer/api/json?pretty=true').pipe(res);
+  request({ url: 'https://ci.nodejs.org/computer/api/json?pretty=true', json: true }, (err, _, apiResponse) => {
+    var offlineComputers = [];
+
+    for(var computer of apiResponse.computer) {
+      if (computer.offline) {
+        offlineComputers.push({
+          name: computer.displayName,
+          reason: computer.offlineCauseReason || 'manually removed'
+        });
+      }
+    }
+
+    res.json({
+      computers: offlineComputers,
+      offlineCount: offlineComputers.length,
+      totalCount: apiResponse.computer.filter((c) => { return c._class === 'hudson.slaves.SlaveComputer' }).length
+    });
+  });
 });
 
 app.get('/queue', function(req, res) {
